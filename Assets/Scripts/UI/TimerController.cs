88173dc70b9baaf43c6d1454832fc679aea8021a
Robin <robin.minervini@gmail.com>
2020-02-15 17:47:52 +0100

    Add: timer

---
+++ Assets/Scripts/UI/TimerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+    #region Fields
+    
+    private TextMeshProUGUI _UITimer;
+    private float _timeElapsed;
+    #endregion
+    
+    void Awake()
+    {
+        _timeElapsed = 0;
+        _UITimer = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void FixedUpdate()
+    {
+        _timeElapsed = Time.fixedTime;
+        _UITimer.SetText("Time : " + _timeElapsed.ToString("0.000"));
+    }
+}
