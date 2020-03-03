0177beb9d675950748f0223f4207b7bdc8634c5f
Omar Jbara <omar.jbara2@gmail.com>
2020-03-03 14:34:24 +0100

    - Fixes after rebase

---
+++ Assets/Scripts/System/Constants.cs
@@ -0,0 +1,4 @@
+    public static class Constants
+    {
+        public static string ANIM_SCENE_TRANSITION_START = "Start";
+    }
--- Assets/Scripts/UI/SceneLoader.cs
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -14,7 +14,7 @@ public class SceneLoader : MonoBehaviour
     {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
         asyncLoad.allowSceneActivation = false;
-        _SceneTransition.SetTrigger("Start");
+        _SceneTransition.SetTrigger(Constants.ANIM_SCENE_TRANSITION_START);
         yield return new WaitForSeconds(transitionTime);
         asyncLoad.allowSceneActivation = true;
     }
