5bbac70106ea11075f996f3d8236e94155d8a5aa
Robin Minervini <robin.minervini@gmail.com>
2020-03-06 11:25:36 +0100

    Add: Remap float extension

--- Assets/Scripts/System/Extensions.cs
+++ Assets/Scripts/System/Extensions.cs
@@ -18,5 +18,20 @@ namespace Rythmformer
             
             return list[rng.Next(list.Count)];
         }
+        
+        public static float Remap (this float from, float fromMin, float fromMax, float toMin,  float toMax)
+        {
+            var fromAbs  =  from - fromMin;
+            var fromMaxAbs = fromMax - fromMin;      
+       
+            var normal = fromAbs / fromMaxAbs;
+ 
+            var toMaxAbs = toMax - toMin;
+            var toAbs = toMaxAbs * normal;
+ 
+            var to = toAbs + toMin;
+       
+            return to;
+        }
     }
 }
