6c660bdc838c3d2dca6af0c5d9bc7b6e0854944a
Robin <robin.minervini@gmail.com>
2020-05-20 12:43:32 +0200

    Add: base score scene

---
+++ Assets/Scripts/UI/ScoreSceneController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSceneController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI ScoreText;
+    [SerializeField] private GameObject _sceneTransition;
+
+    private void Awake()
+    {
+        if (SaveManager.instance.Data.Difficulty == Difficulty.Chill)
+        {
+            ScoreText.gameObject.SetActive(false);
+        }
+    }
+    
+    public void BackToChapter()
+    {
+        StartCoroutine(_sceneTransition.GetComponent<SceneLoader>().LoadLevel("LevelSelector"));
+    }
+}
