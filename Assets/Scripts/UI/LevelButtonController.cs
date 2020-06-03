d5ea7acb3f7ce258bb84ddb71d011828273ec6e7
Robin <robin.minervini@gmail.com>
2020-06-03 21:05:59 +0200

    Basic sun motion on level selector

---
+++ Assets/Scripts/UI/LevelButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class LevelButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    [SerializeField] private Image _banner;
+    [SerializeField] private GameObject _sun;
+    private void Awake()
+    {
+        _sun = GameObject.Find("Sun");
+    }
+
+    public void OnSelect(BaseEventData data)
+    {
+        _sun.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 45);
+    }
+    
+    public void OnDeselect(BaseEventData data)
+    {
+    }
+}
