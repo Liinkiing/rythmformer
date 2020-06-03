fdcfc048587c0381d28007458968893598a53b3f
Robin <robin.minervini@gmail.com>
2020-06-03 18:51:16 +0200

    Add: select animation button

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -1,20 +1,44 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
 
 [ExecuteInEditMode]
-public class ButtonController : MonoBehaviour
+public class ButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
  [SerializeField] private string _textButton;
  [SerializeField] private TextMeshProUGUI _firstLetterTM;
  [SerializeField] private TextMeshProUGUI _textTM;
+ private Shadow _buttonShadow;
+ private int _shadowWidth = 8;
+ private float _hoverAnimationDuration = 0.3f;
 
  private void Awake()
  {
+  _buttonShadow = GetComponent<Shadow>();
   SetButtonText();
+ }
+ 
+ public void OnSelect(BaseEventData data)
+ {
+  transform
+   .DOBlendableMoveBy(new Vector3(-_shadowWidth/2, _shadowWidth/2, 0), _hoverAnimationDuration)
+   .SetEase(Ease.InOutQuint);
+  
+  DOTween
+   .To(()=> _buttonShadow.effectDistance, x=> _buttonShadow.effectDistance = x, new Vector2(_shadowWidth, -_shadowWidth), _hoverAnimationDuration)
+   .SetEase(Ease.InOutQuint);
+ }
+ public void OnDeselect(BaseEventData data)
+ {
+  transform
+   .DOBlendableMoveBy(new Vector3(_shadowWidth/2, -_shadowWidth/2, 0), _hoverAnimationDuration)
+   .SetEase(Ease.InOutQuint);
+  
+  DOTween
+   .To(() => _buttonShadow.effectDistance, x => _buttonShadow.effectDistance = x, Vector2.zero, _hoverAnimationDuration)
+   .SetEase(Ease.InOutQuint);
  }
 
  private void OnValidate()
