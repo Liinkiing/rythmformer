e4d9c1791564e402c5c3af8066953a7b6f1c5e0c
Robin <robin.minervini@gmail.com>
2020-06-12 15:40:30 +0200

    Add: gradient on first letter focused button

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using DG.Tweening;
+using DG.Tweening.Plugins.Options;
 
 [ExecuteInEditMode]
 public class ButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
@@ -13,6 +14,10 @@ public class ButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
  private Shadow _buttonShadow;
  private int _shadowWidth = 8;
  private float _hoverAnimationDuration = 0.3f;
+ private VertexGradient _firstLetterGradient;
+ private Color32 green = new Color32(118, 227, 178, 255);
+ private Color32 lightPurple = new Color32(192, 115, 255, 255);
+ private DG.Tweening.Core.TweenerCore<Color, Color, ColorOptions> _tween;
 
  private void Awake()
  {
@@ -29,6 +34,8 @@ public class ButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
   DOTween
    .To(()=> _buttonShadow.effectDistance, x=> _buttonShadow.effectDistance = x, new Vector2(_shadowWidth, -_shadowWidth), _hoverAnimationDuration)
    .SetEase(Ease.InOutQuint);
+  
+  AnimateFirstLetterGradient();
  }
  public void OnDeselect(BaseEventData data)
  {
@@ -39,6 +46,8 @@ public class ButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
   DOTween
    .To(() => _buttonShadow.effectDistance, x => _buttonShadow.effectDistance = x, Vector2.zero, _hoverAnimationDuration)
    .SetEase(Ease.InOutQuint);
+
+  ResetFirstLetterGradient();
  }
 
  private void OnValidate()
@@ -56,5 +65,107 @@ public class ButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
   
   _firstLetterTM.SetText(firstLetter);
   _textTM.SetText(text);
+ }
+
+ private void AnimateFirstLetterGradient()
+ {
+
+  var stepAnimation = 1;
+  var targetColor = green;
+  var color = lightPurple;
+
+  _tween = DOTween
+    .To(
+     () => color, 
+     x => color = x,
+     targetColor,
+     1f
+     )
+    .SetEase(Ease.Linear)
+    .SetLoops(-1);
+
+  _tween.OnUpdate(() => callback(color, stepAnimation));
+  
+  _tween.OnStepComplete(() =>
+  {
+
+   stepAnimation += 1;
+
+   if (stepAnimation > 4) stepAnimation = 1;
+
+   switch (stepAnimation)
+   {
+    case 1:
+     targetColor = green;
+     color = lightPurple;
+     break;
+    
+    case 2:
+     targetColor = green;
+     color = lightPurple;
+     break;
+    
+    case 3:
+     targetColor = lightPurple;
+     color = green;
+     break;
+    
+    case 4:
+     targetColor = lightPurple;
+     color = green;
+     break;
+   }
+
+   _tween.ChangeStartValue(color);
+   _tween.ChangeEndValue(targetColor);
+  });
+ }
+
+ private void callback(Color32 targetColor, int stepAnimation)
+ {
+  switch (stepAnimation)
+  {
+   case 1:
+    _firstLetterGradient.topLeft = lightPurple;
+    _firstLetterGradient.topRight = lightPurple;
+    _firstLetterGradient.bottomLeft = targetColor;
+    _firstLetterGradient.bottomRight = targetColor;
+    break;
+    
+   case 2:
+    _firstLetterGradient.topLeft = targetColor;
+    _firstLetterGradient.topRight = targetColor;
+    _firstLetterGradient.bottomLeft = green;
+    _firstLetterGradient.bottomRight = green;
+    break;
+    
+   case 3:
+    _firstLetterGradient.topLeft = green;
+    _firstLetterGradient.topRight = green;
+    _firstLetterGradient.bottomLeft = targetColor;
+    _firstLetterGradient.bottomRight = targetColor;
+    break;
+    
+   case 4:
+    _firstLetterGradient.topLeft = targetColor;
+    _firstLetterGradient.topRight = targetColor;
+    _firstLetterGradient.bottomLeft = lightPurple;
+    _firstLetterGradient.bottomRight = lightPurple;
+    break;
+  }
+
+  _firstLetterTM.colorGradient = _firstLetterGradient;
+ }
+
+ private void ResetFirstLetterGradient()
+ {
+  _tween.Kill();
+  
+  _firstLetterGradient.bottomLeft = lightPurple;
+  _firstLetterGradient.bottomRight = lightPurple;
+  _firstLetterGradient.topLeft = lightPurple;
+  _firstLetterGradient.topRight = lightPurple;
+  
+  _firstLetterTM.colorGradient = _firstLetterGradient;
  }
 }
