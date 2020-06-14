788b010393816def8c20b6f5c6da519376589a9e
Omar Jbara <omar.jbara2@gmail.com>
2020-06-14 18:31:18 +0200

    - Added Tuto Binding prefab in level

--- Assets/Scripts/UI/BindingIconButton.cs
+++ Assets/Scripts/UI/BindingIconButton.cs
@@ -6,13 +6,15 @@ public class BindingIconButton : MonoBehaviour
 {
     [SerializeField] private GameAction action;
     private Image _image;
+    private SpriteRenderer _spriteRenderer;
     private BindingIcons _bindingIcons;
 
     private void Awake()
     {
         _image = GetComponent<Image>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         _bindingIcons = UIManager.instance.GetComponent<BindingIcons>();
-        _image.sprite = _bindingIcons.GetSprite(GameManager.instance.CurrentBindingScheme, action);
+        ChangeSprite(GameManager.instance.CurrentBindingScheme);
     }
 
     private void OnEnable()
@@ -27,6 +29,19 @@ public class BindingIconButton : MonoBehaviour
 
     private void OnSchemeChanged(UIManager sender, BindingScheme scheme, string path)
     {
-        _image.sprite = _bindingIcons.GetSprite(scheme, action);
+        ChangeSprite(scheme);
+    }
+
+    private void ChangeSprite(BindingScheme scheme)
+    {
+        if (_image != null)
+        {
+            _image.sprite = _bindingIcons.GetSprite(scheme, action);
+        }
+
+        if (_spriteRenderer != null)
+        {
+            _spriteRenderer.sprite = _bindingIcons.GetSprite(scheme, action);
+        }
     }
 }
