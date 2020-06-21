b70bda8855141686af298b04dc2a67edea4cbb5b
Robin <robin.minervini@gmail.com>
2020-06-21 22:39:53 +0200

    Add: set sprite difficulty button depending of current difficulty

--- Assets/Scripts/UI/DifficultyButtonController.cs
+++ Assets/Scripts/UI/DifficultyButtonController.cs
@@ -9,6 +9,7 @@ public class DifficultyButtonController : MonoBehaviour, ISelectHandler, IDesele
     [SerializeField] private Sprite _disabledSprite;
     [SerializeField] private Image _imageDifficulty;
     [SerializeField] private Material _gradientMaterial;
+    [SerializeField] private Difficulty _difficulty;
     private Shadow _buttonShadow;
     private readonly int _shadowWidth = 8;
     private readonly float _hoverAnimationDuration = 0.3f;
@@ -16,23 +17,46 @@ public class DifficultyButtonController : MonoBehaviour, ISelectHandler, IDesele
     private Color green = new Color32(114, 240,193, 255);
     private Color blue = new Color32(144, 152,232,255);
     private Sequence gradientSequence;
+    private Button _button;
 
     private void Awake()
     {
+        
         _buttonShadow = GetComponent<Shadow>();
+        _button = GetComponent<Button>();
         
         _cloneMaterial = new Material(_gradientMaterial);
         GetComponent<Image>().material = _cloneMaterial;
         
         _cloneMaterial.SetColor("color_top", blue);
         _cloneMaterial.SetColor("color_bottom", green);
+
+        SetSprite();
+        _button.onClick.AddListener(() =>
+        {
+            GameManager.instance.ChangeDifficulty(_difficulty);
+        });
+    }
+    
+    private void OnEnable()
+    {
+        GameManager.instance.DifficultyChanged += OnDifficultyChanged;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.DifficultyChanged -= OnDifficultyChanged;
+    }
+    
+    private void OnDifficultyChanged(GameManager sender, Difficulty difficulty)
+    {
+        SetSprite();
     }
 
     public void OnSelect(BaseEventData data)
     {
         ResetShadowAnimation();
-        _imageDifficulty.sprite = _activatedSprite;
-        
+
         transform
             .DOScale(new Vector3(1.1f,1.1f,1), 0.5f)
             .SetEase(Ease.InOutQuint);
@@ -46,8 +70,7 @@ public class DifficultyButtonController : MonoBehaviour, ISelectHandler, IDesele
     public void OnDeselect(BaseEventData data)
     {
         AnimateShadow();
-        _imageDifficulty.sprite = _disabledSprite;
-        
+
         transform
             .DOScale(new Vector3(1,1,1), 0.5f)
             .SetEase(Ease.InOutQuint);
@@ -61,7 +84,19 @@ public class DifficultyButtonController : MonoBehaviour, ISelectHandler, IDesele
     {
         if (!Application.isPlaying)
         {
+            SetSprite();
+        }
+    }
+    
+    private void SetSprite()
+    {
+        if (SaveManager.instance.Data.Difficulty == _difficulty)
+        {
             _imageDifficulty.sprite = _activatedSprite;
+        }
+        else
+        {
+            _imageDifficulty.sprite = _disabledSprite;
         }
     }
     
